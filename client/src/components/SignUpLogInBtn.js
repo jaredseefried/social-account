e4a2563b1ee866b09241ab0c5fa5f137194ce0a3
Jared Seefried <jared.seefried@yahoo.com>
2021-01-16 22:08:10 -0700

    updated link redirect location path

---
+++ client/src/components/SignUpLogInBtn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignUpLogInBtn() {
+  return (
+    <div className="login-signup-container">
+      <a className="btn btn-primary" href="/signup" type="button">signup</a>
+    </div>
+  )
+}
+
+export default SignUpLogInBtn
